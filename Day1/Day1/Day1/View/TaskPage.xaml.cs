9aeca94b7821cf4c53cf6563f3e3254c848dee17
gplesz <gabor.plesz@gmail.com>
2017-02-21 16:05:30 +0100

    #3: Működő XAML form

--- Day1/Day1/Day1/View/TaskPage.xaml.cs
+++ Day1/Day1/Day1/View/TaskPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Day1.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,51 @@ namespace Day1.View
 {
     public partial class TaskPage : ContentPage
     {
+        private TaskViewModel vm;
+        List<TaskViewModel> list = new List<TaskViewModel>();
         public TaskPage()
         {
             InitializeComponent();
+            ResetData();
         }
+
+        private void ResetData()
+        {
+            vm = new TaskViewModel();
+            vm.Due = DateTime.Now;
+
+            entryTitle.Text = vm.Title;
+            pickerPriority.SelectedIndex = vm.Priority;
+            dueDate.Date = vm.Due.Date;
+            dueTime.Time = vm.Due.TimeOfDay;
+            switchSolved.IsToggled = vm.IsSolved;
+        }
+
+        private void btnList_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new TaskListView(list));
+        }
+
+        private void btnSave_Clicked(object sender, EventArgs e)
+        {
+            var date = dueDate.Date;
+            var time = dueTime.Time;
+            list.Add(new TaskViewModel
+            {
+                Title = entryTitle.Text,
+                Priority = pickerPriority.SelectedIndex,
+                Due = new DateTime(date.Year, date.Month, date.Day, time.Hours, time.Minutes, time.Seconds),
+                IsSolved = switchSolved.IsToggled
+            });
+
+            ResetData();
+        }
+
+        private void btnCancel_Clicked(object sender, EventArgs e)
+        {
+            ResetData();
+        }
+
+
     }
 }

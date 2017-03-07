c444e04afcae5f796f156c028d0696ba6b3bd6ba
gplesz <gabor.plesz@gmail.com>
2017-03-07 16:14:58 +0100

    #4: Új elem felviteléhez gomb a listán + converter

---
+++ Day1/Day1/Day1/Converter/NegateBoolConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Day1.Converter
+{
+    class NegateBoolConverter : IValueConverter
+    {
+        //Megjelenítéskor hívódik meg
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        //bevitelkor a felület->adatforrás bindingnál dolgozik
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
